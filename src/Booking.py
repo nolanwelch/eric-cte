c9ce4f998ff9bf4bc95ec7313853373b74f57501
Nolan <nolanwelch@outlook.com>
2023-08-03 16:32:06 -0400

    Finalize Booking class fields

--- src/Booking.py
+++ src/Booking.py
@@ -3,15 +3,12 @@ from PID import PID
 
 
 class Booking:
-    # TODO: Flesh out this class with all necessary fields. All fields should be public (no name mangling)
     def __init__(
         self,
         id: int,
         start_datetime: datetime.datetime,
-        customer_name: str,
         on_campus_pids: list[PID],
     ):
         self.id = id
         self.start_datetime = start_datetime
-        self.customer_name = customer_name
         self.on_campus_pids = on_campus_pids
