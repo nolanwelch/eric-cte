4334a9511360e7b871e9246671331538915c529b
Nolan <nolanwelch@outlook.com>
2023-08-04 16:11:49 -0400

    Add value checks to PID __init__()

--- src/PID.py
+++ src/PID.py
@@ -1,5 +1,9 @@
 class PID:
     def __init__(self, id: int, first_name: str, last_name: str):
+        if id < 0:
+            raise ValueError("PIDs cannot be negative")
+        elif "" in (first_name, last_name):
+            raise ValueError("Name cannot be empty")
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
