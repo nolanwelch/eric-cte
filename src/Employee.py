4e764529bad7214d29c212efa069f8616fa1bb3d
Nolan <nolanwelch@outlook.com>
2023-08-04 16:16:37 -0400

    Add value checks to Employee __init__()

--- src/Employee.py
+++ src/Employee.py
@@ -1,5 +1,9 @@
 class Employee:
     def __init__(self, first_name: str, last_name: str, employee_id: int):
+        if employee_id < 0:
+            raise ValueError("Employee ID cannot be negative")
+        elif "" in (first_name, last_name):
+            raise ValueError("Name cannot be empty")
         self.first_name = first_name
         self.last_name = last_name
         self.employee_id = employee_id
