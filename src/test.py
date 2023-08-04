07471472b8f5348e4530396e0d27cf527deb9a05
Nolan <nolanwelch@outlook.com>
2023-08-04 14:49:40 -0400

    Add bare test classes

--- src/test.py
+++ src/test.py
@@ -1,13 +1,47 @@
-import os
+import unittest as ut
 
 import app as a
 
+# https://machinelearningmastery.com/a-gentle-introduction-to-unit-testing-in-python/
 
-def test():
+
+class TestSecrets(ut.TestCase):
+    pass
+
+
+class TestBooking(ut.TestCase):
+    pass
+
+
+class TestEmployee(ut.TestCase):
+    pass
+
+
+class TestEvent(ut.TestCase):
+    pass
+
+
+class TestPID(ut.TestCase):
+    pass
+
+
+class TestSingleton(ut.TestCase):
+    pass
+
+
+class TestDatabase(ut.TestCase):
+    pass
+
+
+class TestSlackApp(ut.TestCase):
+    pass
+
+
+class TestSlingApp(ut.TestCase):
     pass
 
 
 if __name__ == "__main__":
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
-    os.chdir("..")
-    test()
+    a.os.chdir(a.os.path.dirname(a.os.path.realpath(__file__)))
+    a.os.chdir("..")
+    ut.main()
