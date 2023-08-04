6340277e0f129b0fd4574b4e93b11f40b5c21a29
Nolan <nolanwelch@outlook.com>
2023-08-04 17:50:39 -0400

    Create dedicated tests/ directory

--- tests/test.py
+++ tests/test.py
@@ -1,37 +1,30 @@
 import os
 import unittest as ut
 
-
 # https://machinelearningmastery.com/a-gentle-introduction-to-unit-testing-in-python/
 
 
 class TestSecrets(ut.TestCase):
     def test_valid_secrets_path(self):
-        from app import get_secrets
+        from src.app import get_secrets
 
         secrets = get_secrets("config.env")
         self.assertIsInstance(secrets, dict)
 
     def test_invalid_secrets_path(self):
-        from app import get_secrets
+        from src.app import get_secrets
 
         with self.assertRaises(OSError):
             get_secrets("invalidpath")
 
     def test_valid_secrets_values(self):
-        from app import validate_secrets
+        from src.app import validate_secrets
 
-        try:
-            validate_secrets({"foo": "bar", "test": "case"}, ["foo", "test"])
-        except Exception as e:
-            self.fail(f"validate_secrets raised {e} for a valid input")
-        try:
-            validate_secrets({"foo": "bar", "test": "case"}, ["foo"])
-        except Exception as e:
-            self.fail(f"validate_secrets raised {e} for input with extra dict pairs")
+        validate_secrets({"foo": "bar", "test": "case"}, ["foo", "test"])
+        validate_secrets({"foo": "bar", "test": "case"}, ["foo"])
 
     def test_invalid_secrets_values(self):
-        from app import validate_secrets
+        from src.app import validate_secrets
 
         with self.assertRaises(KeyError):
             validate_secrets({"foo": "bar"}, ["foo", "test"])
@@ -41,8 +34,9 @@ class TestSecrets(ut.TestCase):
 
 class TestBooking(ut.TestCase):
     def test_valid_init(self):
-        from Booking import Booking
         from datetime import datetime
+
+        from src.Booking import Booking
 
         now = datetime.now()
         booking = Booking(123456789, now, [])
@@ -51,8 +45,9 @@ class TestBooking(ut.TestCase):
         self.assertEqual(booking.on_campus_pids, [])
 
     def test_invalid_init(self):
-        from Booking import Booking
         from datetime import datetime
+
+        from src.Booking import Booking
 
         now = datetime.now()
         with self.assertRaises(ValueError):
@@ -63,7 +58,7 @@ class TestBooking(ut.TestCase):
 
 class TestEmployee(ut.TestCase):
     def test_valid_init(self):
-        from Employee import Employee
+        from src.Employee import Employee
 
         employee = Employee("Foo", "Bar", 123456789)
         self.assertEqual(employee.first_name, "Foo")
@@ -71,7 +66,7 @@ class TestEmployee(ut.TestCase):
         self.assertEqual(employee.employee_id, 123456789)
 
     def test_invalid_init(self):
-        from Employee import Employee
+        from src.Employee import Employee
 
         with self.assertRaises(ValueError):
             Employee("", "Bar", 123456789)
@@ -87,7 +82,7 @@ class TestEvent(ut.TestCase):
 
 class TestPID(ut.TestCase):
     def test_valid_init(self):
-        from PID import PID
+        from src.PID import PID
 
         pid = PID(123456789, "Foo", "Bar")
         self.assertEqual(pid.id, 123456789)
@@ -95,7 +90,7 @@ class TestPID(ut.TestCase):
         self.assertEqual(pid.last_name, "Bar")
 
     def test_invalid_init(self):
-        from PID import PID
+        from src.PID import PID
 
         with self.assertRaises(ValueError):
             PID(-1, "Foo", "Bar")
@@ -107,7 +102,7 @@ class TestPID(ut.TestCase):
 
 class TestSingleton(ut.TestCase):
     def test_unique_instance(self):
-        from Singleton import Singleton
+        from src.Singleton import Singleton
 
         class TestClass(metaclass=Singleton):
             pass
@@ -119,17 +114,38 @@ class TestSingleton(ut.TestCase):
 
 class TestDatabase(ut.TestCase):
     def test_valid_init(self):
-        from logging import Logger
+        import logging
 
-        try:
-            pass
-        except Exception as e:
-            self.fail(f"Database.__init__() raised {e} for a valid input")
+        from src.app import get_secrets, validate_secrets
+        from src.Database import Database
+        from src.Secrets import secret_keys
+
+        logger = logging.Logger("test", logging.INFO)
+        s = get_secrets("config.env")
+        validate_secrets(s, secret_keys)
+
+        db = Database(logger, s["CTE_DB_PATH"], s["CAMPUS_ROSTER_PATH"], "X", "X")
+        self.assertIs(db._logger, logger)
+        self.assertEqual(db._bookeo_api_key, "X")
+        self.assertEqual(db._bookeo_secret_key, "X")
+        self.assertEqual(db._db_filepath, "data/cte.sqlite3")
+        self.assertEqual(db._roster_filepath, "data/roster.csv")
 
     def test_invalid_init(self):
         from logging import Logger
 
+        from src.Database import Database
+
         logger = Logger("test")
+
+        with self.assertRaises(OSError):
+            Database(logger, "invalidpath", "data/roster.csv", "X", "X")
+        with self.assertRaises(OSError):
+            Database(logger, "data/cte.sqlite3", "invalidpath", "X", "X")
+        with self.assertRaises(ValueError):
+            Database(logger, "data/cte.sqlite3", "data/roster.csv", "", "")
+        with self.assertRaises(IOError):
+            Database(logger, "data/roster.csv", "data/roster.csv", "X", "X")
 
 
 class TestSlackApp(ut.TestCase):
