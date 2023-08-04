3f5e36f1c83e2402c41aa2176a34f32aae65f814
Nolan <nolanwelch@outlook.com>
2023-08-04 16:18:41 -0400

    Add value checks to Booking __init__()

--- src/Booking.py
+++ src/Booking.py
@@ -9,6 +9,10 @@ class Booking:
         start_datetime: datetime.datetime,
         on_campus_pids: list[PID],
     ):
+        if id < 0:
+            raise ValueError("Booking ID cannot be negative")
+        elif start_datetime is None:
+            raise TypeError("Starting datetime cannot be None")
         self.id = id
         self.start_datetime = start_datetime
         self.on_campus_pids = on_campus_pids
