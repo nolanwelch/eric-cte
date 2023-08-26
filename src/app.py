86b9c5350e8509be83a8a017f3ec7dd872efd38e
Nolan <nolanwelch@outlook.com>
2023-08-26 19:22:05 -0400

    Make barebones app.main()

--- src/app.py
+++ src/app.py
@@ -1,121 +1,122 @@
+import datetime as dt
 import logging
 import os
-import time
-from datetime import timedelta
+from logging.handlers import TimedRotatingFileHandler
+from time import sleep
 
+import pytz
+import requests
 from Booking import Booking
 from Database import Database
 from dotenv import dotenv_values
-from Event import Event
+from Secrets import secret_keys
 from SlackApp import SlackApp
-from SlingApp import SlingApp
 
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
+LOCAL_TIMEZONE = pytz.timezone("America/New_York")
+USERAGENT = "ERIC-CTE/1.0 (nolanwelch@outlook.com)"
 
 # TODO
-# - Get shifts from Sling for given date
-# - Listen for Slack reactions and check against ID
-# - Perform on-campus PID checks when new booking detected/sent
-# - Figure out why logging isn't printing
+# - Add functionality to check for updates made to bookings (namely, new/modified PIDs)
+# - Write tests for all classes and methods
+# - Restructure try/catch and Exception raising (https://stackoverflow.com/a/18679131/8344620))
+
+# TODO: Future plans
+# - Fetch emails via SMTP to update local on-campus roster
 
 
 def get_secrets(filepath: str) -> dict[str, str | None]:
     if not os.path.exists(filepath):
-        raise Exception(f"Filepath {filepath} does not exist")
+        raise OSError(f"Filepath {filepath} does not exist")
     return dotenv_values(filepath)
 
 
-def validate_secrets(secrets: dict) -> None:
-    keys = [
-        "BOOKEO_API_KEY",
-        "BOOKEO_SECRET_KEY",
-        "CAMPUS_ROSTER_PATH",
-        "CTE_DB_PATH",
-        "SLACK_BOT_TOKEN",
-        "SLACK_SIGNING_SECRET",
-        "SLING_USERNAME",
-        "SLING_PASSWORD",
-        "MSG_QUEUE_PATH",
-        "LOG_PATH",
-    ]
+def validate_secrets(secret_dict: dict, keys: list[str]) -> None:
     for s in keys:
-        if s not in secrets.keys():
+        if s not in secret_dict.keys():
             raise KeyError(f"Secret {s} not found in config")
-        elif not secrets[s]:
+        elif not secret_dict[s]:
             raise ValueError(f"Value for secret {s} is empty")
     logging.info("Secrets validated")
 
 
-def main():
-    secrets = get_secrets("config.env")
-    validate_secrets(secrets)
+def connected_to_internet() -> bool:
+    try:
+        _ = requests.head("http://www.google.com", timeout=5)
+        return True
+    except:
+        return False
 
-    sling = SlingApp(secrets["SLING_USERNAME"], secrets["SLING_PASSWORD"])
+
+def main():
+    logger = logging.getLogger("eric-cte")
+    secrets = get_secrets("config.env")
+    validate_secrets(secrets, secret_keys)
+
+    fh = TimedRotatingFileHandler(
+        filename=secrets["LOG_PATH"],
+        when="midnight",
+        backupCount=60,
+        encoding="utf-8",
+    )
+    fh.setFormatter(
+        logging.Formatter("%(asctime)s;%(levelname)s;%(message)s", "%Y-%m-%d %H:%M:%S")
+    )
+    fh.setLevel(logging.INFO)
+    logger.addHandler(fh)
+
+    slack = SlackApp(
+        logger,
+        secrets["SLACK_BOT_TOKEN"],
+        quiet_hours_start=dt.time(hour=21),
+        quiet_hours_end=dt.time(hour=8),
+    )
     db = Database(
+        logger,
         secrets["CTE_DB_PATH"],
         secrets["CAMPUS_ROSTER_PATH"],
         secrets["BOOKEO_SECRET_KEY"],
         secrets["BOOKEO_API_KEY"],
     )
-    slack = SlackApp(
-        secrets["SLACK_BOT_TOKEN"],
-        secrets["SLACK_SIGNING_SECRET"],
-        secrets["MSG_QUEUE_PATH"],
-    )
-
-    last_queue_check = -1
-    last_booking_fetch = -1
 
     admins = db.get_admins()
-    admin_ids = [a.slack_id for a in admins]
+    admin_slack_ids = [db.get_slack_id(a.employee_id) for a in admins]
+
+    last_fetch = dt.datetime.fromtimestamp(0)
 
     while True:
-        # TODO: Organize code
-        slack.send_queued_messages()
-        new_bookings = db.fetch_bookings(timedelta(weeks=1))
-        for b in new_bookings:
-            b["employeeID"] = sling.fetch_employee_id(b["startTime"])
-        db.update_database(new_bookings)
-        new_pids = db.fetch_new_on_campus_pids()
-        invalid_pids = [p for p in new_pids if not db.is_on_campus_student(p)]
-        if invalid_pids:
-            # Make note in DB that admin has been notified
-            # Notify admin
-            slack.send_multiple(admin_ids, "test")
-            # for a in admins:
-            #     # TODO: Add system to tie booking ID and names to PIDs. Use SQL queries.
-            #     slack.send_message(
-            #         a["slackID"],
-            #         f"Hey {a['firstName']}, there are some invalid on-campus PIDs in the following bookings {0}. They are: {0}",
-            #     )
-        for b in new_bookings:
-            # TODO: For each new booking, notify the employee assigned to the shift
-            pass
+        while not connected_to_internet():
+            logger.error("No Internet connection")
+            sleep(60)
 
-        for e in slack.get_new_events():
-            e.handle(admin_ids)
+        # Update local database
+        db.clear()
+        fetch_delta = dt.timedelta(days=31)
+        bookings: list[Booking] = []
+        if dt.datetime.now() - last_fetch > dt.timedelta(minutes=5):
+            last_fetch = dt.datetime.now()
+            bookings = db.fetch_bookings(fetch_delta)
+            db.insert_new_bookings(bookings)
 
-        time.sleep(30)
+        for b in bookings:
+            booking_datetime = b.start.astimezone(LOCAL_TIMEZONE)
+            booking_date = booking_datetime.strftime("%A, %B %-d")
+            # Check for invalid on-campus PIDs
+            pids = db.get_on_campus_pids(b.id)
+            pids = [p for p in pids if p != db.get_matching_pid(p)]
+            if not db.admin_notified_pids(b) and pids:
+                m = f":x: There are some invalid on-campus PIDs in booking *{b.id}* on {booking_date}. "
+                m += f"They are: {', '.join(f'*{p.id}* ({p.last_name}, {p.first_name})' for p in pids)}. "
+                m += f"Contact email: {b.email}"
+                slack.send_multiple(admin_slack_ids, m)
+                db.mark_admin_notified_pids(b)
+                for p in pids:
+                    db.remove_pid(p)
 
-
-# def test():
-#     secrets = get_secrets("config.env")
-#     validate_secrets(secrets)
-#     slack = SlackApp(
-#         secrets["SLACK_BOT_TOKEN"],
-#         secrets["SLACK_SIGNING_SECRET"],
-#         secrets["MSG_QUEUE_PATH"],
-#     )
-#     slack.send_message("U05F0L9LN3G", "This is a test!")
-#     # sling = SlingApp(
-#     #     secrets["SLING_USERNAME"], secrets["SLING_PASSWORD"], SLING_TTL_SECS
-#     # )
-#     # dt = datetime.fromtimestamp(1688139900).strftime(r"%Y-%m-%dT%H:%M:00Z")
-#     # print(dt)
-#     # print(sling.fetch_employee_id(dt))
-#     sys.exit()
+        sleep(30)
 
 
 if __name__ == "__main__":
-    # test()
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+    os.chdir("..")
+    logging.basicConfig(level=logging.INFO)
     main()
